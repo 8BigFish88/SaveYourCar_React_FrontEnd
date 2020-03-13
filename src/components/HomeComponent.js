639221f9982a7394a723fd8ee2a710fabb713b90
8BigFish88 <ivana.zaccheddu88@gmail.com>
2020-03-13 11:38:41 +0100

    add reminders

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+function Cars({ id }) {
+  const [items, setItems] = useState({ reminders_count: 0, cars: [] });
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const fetchItems = async () => {
+    let data = [];
+    setLoading(true);
+    while (true) {
+      data = await fetch(
+        `https://saveyourcar-api.herokuapp.com/api/v1.0/reminders/user/${id}`
+      );
+      const items = await data.json();
+      console.log(items.data);
+      if (data.status === 200) {
+        setItems(items);
+        setLoading(false);
+        console.log(items.count);
+        items.reminders_count > 0 &&
+          alert(
+            `Hai ${items.reminders_count} reminders in sospeso! Controlla le tue auto!`
+          );
+        break;
+      }
+    }
+  };
+
+  return (
+    <div className="jumbotron mt-5 ">
+      <h1>Le tue Auto:</h1>
+      <div className="album py-5">
+        <div className="row">
+          {loading ? (
+            <h1 className="col-lg-12">...loading content</h1>
+          ) : (
+            items.cars.map(item => {
+              return (
+                <div className="mb-4 col-lg-3" key={item.id}>
+                  <Link to={`/users/${id}/cars/${item.id}`}>
+                    <button
+                      onClick={() => {
+                        item.reminders &&
+                          Object.values(item.reminders_text).map(text => {
+                            alert(text);
+                          });
+                      }}
+                      className="btn btn-block btn-secondary m-2 "
+                    >
+                      {item.name}
+                    </button>
+                    <span
+                      onClick={() => {
+                        item.reminders &&
+                          Object.values(item.reminders_text).map(text => {
+                            alert(text);
+                          });
+                      }}
+                      className="badge badge-secondary"
+                    >
+                      {item.reminders}
+                    </span>
+                  </Link>
+                </div>
+              );
+            })
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cars;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,20 +1,24 @@
 import React from "react";
 import auth from "../auth";
 import { Link } from "react-router-dom";
+import HomeComponent from "../components/HomeComponent";
 
 const Home = props => {
   return (
     <div className="jumbotron">
       <h1>Home Page</h1>
       {auth.isAuthenticated() ? (
-        <button
-          className="btn btn-secondary m-2"
-          onClick={() => {
-            auth.logout(() => props.history.push("/"));
-          }}
-        >
-          LogOut
-        </button>
+        <React.Fragment>
+          <button
+            className="btn btn-secondary m-2"
+            onClick={() => {
+              auth.logout(() => props.history.push("/"));
+            }}
+          >
+            LogOut
+          </button>
+          <HomeComponent id={auth.getId()} />
+        </React.Fragment>
       ) : (
         <Link to="/login">
           <button className="btn btn-secondary m-2">LogIn</button>
